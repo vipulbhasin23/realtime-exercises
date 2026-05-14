95dadcc1cbbf3798b269c455317f3b5aa3f7de3a
Vipul Bhasin <vipulbhasin22@gmail.com>
2026-05-14 10:38:45 +0530

    Replace setTimeout with requestAnimationFrame
    Signed-off-by: Vipul Bhasin <vipulbhasin22@gmail.com>

--- polling/exercise/frontend/polling-chat.js
+++ polling/exercise/frontend/polling-chat.js
@@ -42,7 +42,6 @@ async function getNewMsgs() {
   }
   allChat = json.msg;
   render();
-  setTimeout(getNewMsgs, INTERVAL);
 }
 
 function render() {
@@ -58,5 +57,15 @@ function render() {
 const template = (user, msg) =>
   `<li class="collection-item"><span class="badge">${user}</span>${msg}</li>`;
 
-// make the first request
-getNewMsgs();
+let timeToMakeNextRequest = 0;
+async function rafTimer(time) {
+  console.log(time);
+  if (timeToMakeNextRequest <= time) {
+    await getNewMsgs();
+    timeToMakeNextRequest = time + INTERVAL;
+  }
+
+  requestAnimationFrame(rafTimer);
+}
+
+requestAnimationFrame(rafTimer);
